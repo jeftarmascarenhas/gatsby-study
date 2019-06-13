b00da522f7192e586e1f821b22445e0ae5938e53
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2019-06-13 13:59:06 -0300

    using css module in gatsby

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,36 @@
 import React from 'react'
 import Header from '../components/header'
-import Layout from '../components/layout'
+import Container from '../components/container'
+
+import aboutStyles from './about.module.css'
+
+const User = ({
+  avatar,
+  username,
+  excerpt,
+}) => (
+  <div className={aboutStyles.user}>
+    <img src={avatar} className={aboutStyles.avatar} alt="" />
+    <div className={aboutStyles.description}>
+      <h2 className={aboutStyles.username}>{username}</h2>
+      <p className={aboutStyles.excerpt}>{excerpt}</p>
+    </div>
+  </div>
+)
 
 export default () => (
-  <Layout>
+  <Container>
     <Header siteTitle="About Gatsby" />
     <p>Such wow. Very React.</p>
-  </Layout>
+    <User
+      username="Jane Doe"
+      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+      excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
+    <User
+      username="Bob Smith"
+      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+      excerpt="I'm Bob Smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
+  </Container>
 )
